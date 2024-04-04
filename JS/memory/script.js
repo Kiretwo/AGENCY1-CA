e34480e0a9ae658abfc9e50eb5625a14d4684b7a
Erik <eritor00991@stud.noroff.no>
2024-04-04 07:06:19 +0200

    7:51

---
+++ JS/memory/script.js
@@ -0,0 +1,17 @@
+const moves = document.getElementById("movesCount");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-container");
+
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
+// Items array
+const items = [
+  {name:"bee", image:"bee.png"}
+]
