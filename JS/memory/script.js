f8f20f498d92b2a84da33d0dd6a0963f4e2aebdd
Erik <eritor00991@stud.noroff.no>
2024-04-08 19:20:52 +0200

    20:07

--- JS/memory/script.js
+++ JS/memory/script.js
@@ -13,5 +13,82 @@ let secondCard = false;
 
 // Items array
 const items = [
-  {name:"bee", image:"bee.png"}
-]
+  {name:"chicken", image:"chicken.png"},
+  {name:"cow", image:"cow.png"},
+  {name:"crab", image:"crab.png"},
+  {name:"fox", image:"fox.png"},
+  {name:"hedgehog", image:"hedgehog.png"},
+  {name:"hippo", image:"hippo.png"},
+  {name:"koala", image:"koala.png"},
+  {name:"lemur", image:"lemur.png"},
+  {name:"pig", image:"pig.png"},
+  {name:"tiger", image:"tiger.png"},
+  {name:"whale", image:"whale.png"},
+  {name:"zebra", image:"zebra.png"},
+];
+
+// Initial Time
+let seconds = 0,
+  minutes = 0;
+// Initial moves and win count
+let movesCount = 0,
+  winCount = 0;
+
+// For timer
+const timeGenerator = () => {
+  seconds+= 1;
+  // minutes logic
+  if(seconds >= 60) {
+    minutes += 1;
+    seconds = 0;
+  }
+  // format time before displaying
+  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+  let minutesValues = minutes < 10 ? `0${minutes}` : minutes;
+  timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
+
+//For calculating moves
+const movesCounter = () => {
+  movesCount += 1;
+  moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+};
+
+//Pick random objects from the items array
+const generateRandom = (size = 4) => {
+  //temporary array
+  let tempArray = [...items];
+  //initializes cardValues array
+  let cardValues = [];
+  //size should be double (4*4 matrix)/2 since pairs of objects would exist
+  size= (size * size) / 2;
+  //Random object selection
+  for(let i = 0; i < size; i++) {
+    const randomIndex = Math.floor(Math.random() * tempArray.length);
+    cardValues.push(tempArray[randomIndex]);
+    //once selected remove the object from temp array
+    tempArray.splice(randomIndex, 1);
+  }
+  return cardValues;
+};
+
+const matrixGenerator = (cardValues, size = 4) => {
+  gameContainer.innerHTML = "";
+  cardValues = [...cardValues, ...cardValues];
+  //simple shuffle
+  cardValues.sort(() => Math.random() - 0.5);
+  for(let i=0; i < size*size; i++){
+
+  }
+};
+
+//Initialize values and func calls
+const initializer = () => {
+  result.innerText = "";
+  winCount = 0;
+  let cardValues = generateRandom();
+  console.log(cardValues);
+  matrixGenerator(cardValues);
+};
+
+initializer();
